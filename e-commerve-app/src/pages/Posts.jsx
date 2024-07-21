521d573f9de165af341f286ffc6d15591f6f2e59
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-22 01:05:13 +0300

    adding loaders to posts page

--- e-commerve-app/src/pages/Posts.jsx
+++ e-commerve-app/src/pages/Posts.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import LottieAnimation1 from "../animations/LottieAnimation1";
 
-////////////////////////////////////////
+
 function Posts() {
-  const [s, setPosts] = useState([]); 
+  const [postsList, setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(json => setPosts(json))
-  }, [])
-    return (
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((json) => setPosts(json));
+  }, []);
+  return (
     <main className="posts">
       <div className="pg-header">
         <div className="container">
@@ -17,31 +17,30 @@ function Posts() {
             <div className="col-lg-7">
               <h1>Posts</h1>
             </div>
-            {
-              s.map(post => {
+            {postsList.length === 0 ? (
+              <LottieAnimation1 />
+            ) : (
+              postsList.map((post) => {
                 return (
-                  <div className='col-sm-6' key={post.id}>
-                    <div className='card'>
-                    <div className='card-body'>
-                    <h5 className='card-title'>{post.title}</h5>
-                    <h6 className='card-test'>{post.body}</h6>
-                    </div>
+                  <div className="col-sm-6" key={post.id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">{post.title}</h5>
+                        <h6 className="card-test">{post.body}</h6>
+                      </div>
                     </div>
                   </div>
-                )
+                );
               })
-            }
+            )}
           </div>
         </div>
       </div>
       <div className="container content">
-        
-            <div className="row">
-            </div>
-      
+        <div className="row"></div>
       </div>
     </main>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
