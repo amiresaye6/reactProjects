85fe39066602e36381e8c3ce2a7f3c92c37fbb0c
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-25 19:05:20 +0300

    edit app, index

--- e-commerve-app/src/App.js
+++ e-commerve-app/src/App.js
@@ -1,29 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
-
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Posts from './pages/Posts.jsx';
-import Products from './pages/Products.jsx';
-import SingleProduct from './pages/SingleProduct.jsx';
-import Error from './pages/Error.jsx';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductsList from "./components/ProductsList";
+import Slider from "./components/Slider";
+import About from "./components/About";
+import ErrorComponent from "./components/ErrorComponent";
+import ProductDetails from "./components/ProductDetails";
+import FilteredProductsPerCategory from "./components/FilteredProductsPerCategory";
 
 function App() {
 	return (
 		<div className='App'>
-			<Header />
+			<Navbar />
 			<Routes>
-				<Route element={ <Home /> } path='/'></Route>
-				<Route element={ <About /> } path='/about'></Route>
-				<Route element={ <Posts /> } path='/posts'></Route>
-				<Route element={ <Products /> } path='/products'></Route>
-				<Route element={ <SingleProduct /> } path='/products/:productid'></Route>
-				<Route element={ <Error /> } path='*'></Route>
+				<Route path="/" element={
+					<>
+						<Slider />
+						<ProductsList />
+					</>
+				} />
+				<Route path="/about" element={<About />} />
+				<Route path="products/:productId" element={<ProductDetails />} />
+				<Route path="products/category/:categoryName" element={<FilteredProductsPerCategory />} />
+				<Route path="*" element={<ErrorComponent />} />
 			</Routes>
-			<Footer />
 		</div>
 	);
 }
--- e-commerve-app/src/index.js
+++ e-commerve-app/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
