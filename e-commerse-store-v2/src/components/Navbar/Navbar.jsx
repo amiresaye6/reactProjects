6d60fe58cd2742bf3d0b8c5228998e6b91bc84b8
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-28 17:46:14 +0300

    updating navbar componunt

--- e-commerse-store-v2/src/components/Navbar/Navbar.jsx
+++ e-commerse-store-v2/src/components/Navbar/Navbar.jsx
@@ -3,37 +3,37 @@ import logo from '../Assets/logo.png';
 import cartIcon from '../Assets/cart_icon.png';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     const [menu, setMenu] = useState('');
     return (
         <nav className="navbar">
             <div className="navbar__container">
-                <div className='navbar__logo'>
-                <img src={logo} alt="logo" />
-                <p>SHOPFY</p>
-                </div>
+                <Link to='/' className='navbar__logo'>
+                    <img src={logo} alt="logo" />
+                    <p>SHOPFY</p>
+                </Link>
                 <ul className="navbar__menu">
                     <Link to='/shop' onClick={() => setMenu('Shop')} className="navbar__item">
-                        Shop{menu === 'Shop' && <hr/>}
+                        Shop{menu === 'Shop' && <hr />}
                     </Link>
                     <Link to='/men' onClick={() => setMenu('Men')} className="navbar__item">
-                        Men{menu === 'Men' && <hr/>}
+                        Men{menu === 'Men' && <hr />}
                     </Link>
                     <Link to='/women' onClick={() => setMenu('Women')} className="navbar__item">
-                        Women{menu === 'Women' && <hr/>}
+                        Women{menu === 'Women' && <hr />}
                     </Link>
                     <Link to='/kids' onClick={() => setMenu('Kids')} className="navbar__item">
-                        Kids{menu === 'Kids' && <hr/>}
+                        Kids{menu === 'Kids' && <hr />}
                     </Link>
                 </ul>
                 <div className="navbar_login_cart">
                     <button className="navbar__login">Login</button>
-                    <img src={cartIcon} className="navbar__cart"/>
+                    <img src={cartIcon} className="navbar__cart" alt="Cart" />
                 </div>
             </div>
         </nav>
     );
 }
-
 
 export default Navbar;
