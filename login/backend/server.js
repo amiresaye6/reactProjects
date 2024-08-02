89ce908c729bccd1eea3c9f43306d1bcbb1d220d
Amir Alsayed <amyralsyd367@gmail.com>
2024-08-02 22:44:14 +0300

    adding env for security

--- login/backend/server.js
+++ login/backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
@@ -8,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'testuser',
-    password: 'Amir@mysql1',
-    database: 'alx_graduation_test'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
