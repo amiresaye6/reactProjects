38030571074cc9a3102e7602bdfd0a3bb4a2b3f5
Amir Alsayed <amyralsyd367@gmail.com>
2024-08-30 20:56:57 +0300

    security leak

--- fullStak/backend/config.js
+++ fullStak/backend/config.js
@@ -1,8 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  // 'mongodb://localhost:27017/bookStorDB';
-  'mongodb+srv://amiralsayedwork:6zdLYJMajODV9jCW@backenddb.cp7te.mongodb.net/depi-api?retryWrites=true&w=majority&appName=BackendDb';
+  'mongodb://localhost:27017/bookStorDB';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
